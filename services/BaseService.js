fc3876afa5faa02d56fd42e21e7d53e31243fc08
unknown <dtbinh59@gmail.com>
2021-01-25 19:36:47 +0700

    backup_toan_bo_source_code_v1

---
+++ services/BaseService.js
@@ -0,0 +1,36 @@
+export class BaseService{
+    constructor(){
+
+    }
+    post = (url,data) =>{
+        const promise = axios ({
+            url:url,
+            method: 'POST',
+            data:data
+        });
+        return promise;
+    };
+    get = (url,data)=>{
+        const promise = axios({
+            url:url,
+            method:'GET',
+            data:data
+        });
+        return promise;
+    };
+    put = (url,data) =>{
+        const promise = axios({
+            url:url,
+            method:'PUT',
+            data:data
+        });
+        return promise;
+    };
+    delete = (url) =>{
+        const promise = axios({
+            url:url,
+            method:'DELETE'
+        })
+        return promise
+    }
+}
